7538a808e9a8e95920a06ea221b1f18fe1e9db21
William Robertson <williamp.robertson@gmail.com>
2018-03-31 14:21:41 -0700

    navar up

---
+++ client/src/components/NavBar/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="navbar navbar-inverse navbar-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="collapsed navbar-toggle">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" /> <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a href="/" className="navbar-brand">
+          Turntly
+        </a>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
