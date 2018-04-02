02c5bc238d7fe62581925fb6f5a6f191b4eefb1d
William Robertson <williamp.robertson@gmail.com>
2018-04-01 23:24:32 -0700

    navbar color updates, logo not working yet

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -1,4 +1,10 @@
 import React from "react";
+import "./Navbar.css";
+import logo from "./logo.png";
+
+function Brand () {
+  return <img src={logo} alt="Logo" />;
+}
 
 const Navbar = () => (
   <nav className="navbar navbar-inverse navbar-top">
@@ -6,7 +12,7 @@ const Navbar = () => (
       <div className="navbar-header">
         <button type="button" className="collapsed navbar-toggle">
           <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" /> <span className="icon-bar" />
+          <span className="icon-bar" /><span className="icon-bar" />
           <span className="icon-bar" />
         </button>
         <a href="/" className="navbar-brand">
