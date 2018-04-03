813e7ee95797060d008be78b0efa910e0c7f0e91
William Robertson <williamp.robertson@gmail.com>
2018-04-02 23:17:42 -0700

    added chatcard to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Chatcard from "./components/Chatcard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Chatcard />
       </div>
     );
   }
