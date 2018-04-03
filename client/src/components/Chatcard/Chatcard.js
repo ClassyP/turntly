12d68ed9089b486b253c190cd80b246620e86755
William Robertson <williamp.robertson@gmail.com>
2018-04-03 02:21:16 -0700

    components built for chatcard and initial styling

--- client/src/components/Chatcard/Chatcard.js
+++ client/src/components/Chatcard/Chatcard.js
@@ -1,7 +1,48 @@
 import React from "react";
 import "./Chatcard.css";
+import event from "./event.png";
 import { Panel } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+
+// function to bring in event specific info: event photo,location
+class EventDetails extends React.Component {
+  render() {
+    return (
+      <div>
+        <EventImage />
+        <br />Location: <EventLocation />
+      </div>
+    );
+  }
+}
+
+// will need custom logic to load in event name dynamically
+class Event extends React.Component {
+  render() {
+    return <div>Event Name</div>;
+  }
+}
+
+// will need custom logic to load in event location dynamically
+class EventLocation extends React.Component {
+  render() {
+    return <span>The Van Buren</span>;
+  }
+}
+
+// will need custom logic to load in event image dynamically
+class EventImage extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="#">
+          <Image src={event} alt="event" responsive />
+        </a>
+      </div>
+    );
+  }
+}
 
 const Chatcard = () => (
   <Row className="show-grid">
@@ -12,7 +53,10 @@ const Chatcard = () => (
             <Panel.Heading>
               <Panel.Title componentClass="h3">Event 1</Panel.Title>
             </Panel.Heading>
-            <Panel.Body>Panel content</Panel.Body>
+            <Panel.Body>
+              <Event />
+              <EventDetails />
+            </Panel.Body>
           </Panel>
         </Col>
         <Col xs={6} md={4}>
@@ -20,7 +64,10 @@ const Chatcard = () => (
             <Panel.Heading>
               <Panel.Title componentClass="h3">Event 2</Panel.Title>
             </Panel.Heading>
-            <Panel.Body>Panel content</Panel.Body>
+            <Panel.Body>
+              <Event />
+              <EventDetails />
+            </Panel.Body>
           </Panel>
         </Col>
         <Col xsHidden md={4}>
@@ -28,7 +75,10 @@ const Chatcard = () => (
             <Panel.Heading>
               <Panel.Title componentClass="h3">Event 3</Panel.Title>
             </Panel.Heading>
-            <Panel.Body>Panel content</Panel.Body>
+            <Panel.Body>
+              <Event />
+              <EventDetails />
+            </Panel.Body>
           </Panel>
         </Col>
       </Row>
