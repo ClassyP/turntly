741f8efed60de3dcfd88056441d657e770bcab69
classyp <jeanphilip@mecom>
2018-04-10 11:25:03 -0700

    added send field to chatcard

--- src/Chatcard/Chatcard.js
+++ src/Chatcard/Chatcard.js
@@ -5,13 +5,13 @@ import { Card, CardImg, CardText, CardBody,
 const Chatcard = (props) => {
   return (
     <div>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+      <Card id="chatcard">
+        <CardImg top width="20%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+          <input type="text"></input><Button>send</Button>
         </CardBody>
       </Card>
     </div>
