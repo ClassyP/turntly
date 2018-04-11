eaae95062a5a6b6e5bbff924544d49c1b6173769
classyp <jeanphilip@mecom>
2018-04-10 21:16:18 -0700

    added logo to app

--- src/Chatcard/Chatcard.js
+++ src/Chatcard/Chatcard.js
@@ -6,7 +6,7 @@ const Chatcard = (props) => {
   return (
     <div>
       <Card id="chatcard">
-        <CardImg top width="20%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardImg top width="20%" width="50%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
