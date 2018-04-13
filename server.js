554e94a647f2a35a8b095b2ce6a1d350f1e62b33
William Robertson <williamp.robertson@gmail.com>
2018-04-12 21:16:04 -0700

    updates

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 const cookieSession = require('cookie-session');
@@ -13,19 +12,13 @@ const f = require("util").format;
 const PORT = process.env.PORT || 3001;
 const keys = require('./config/keys.js');
 
-=======
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
->>>>>>> 05dacea2961f420418737c57897ccfdceaf347a4
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-<<<<<<< HEAD
 app.use(bodyParser.json());
 app.use(
   cookieSession({
-    maxAge: 30 * 24 *60 * 60 * 1000,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
@@ -42,22 +35,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //This is our server listener
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
-=======
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
->>>>>>> 05dacea2961f420418737c57897ccfdceaf347a4
