05dacea2961f420418737c57897ccfdceaf347a4
William Robertson <williamp.robertson@gmail.com>
2018-04-12 21:02:27 -0700

    login modal added

---
+++ src/Loginmodal/Loginmodal.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class Loginmodel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="primary" onClick={this.toggle}>
+          {this.props.buttonLabel}Login
+        </Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Login</ModalHeader>
+          <ModalBody>Form will be here</ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle} href="/auth/google">
+              Google Login
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Loginmodel;
