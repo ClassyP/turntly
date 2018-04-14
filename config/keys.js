5708a352fdbab43664f6b5079eea6a85b825d7a5
Manuel Romo <rupertromeo86@gmail.com>
2018-04-14 12:48:43 -0700

    adding oauth pieces to this branch

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+exports.google = {
+  id: process.env.GOOGLE_ID,
+  secret: process.env.GOOGLE_SECRET,
+  cookie_key: process.env.COOKIE_KEY,
+  redirect_domain: process.env.REDIRECT_DOMAIN
+};
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const path = require("path");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
 const MongoClient = require("mongodb").MongoClient;
 const f = require("util").format;
 const assert = require("assert");
+const keys = require('./config/keys.js');
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 dotenv.load();
 
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
