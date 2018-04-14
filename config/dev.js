608c19d21e3c10a16ebf7be4cea711b7fbab5cc3
Manuel Romo <rupertromeo86@gmail.com>
2018-04-14 10:52:37 -0700

    added dev keys

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//These are our production keys
+module.exports = {
+    googleClientID: 4717089349368f7vh46cj86m7av1kmdbc2gukbea9kf0.apps.googleusercontent.com,
+    googleClientSecret: W0RVPQOxzWjaIFZxzk2HLL0V,
+    cookieKey: process.env.COOKIE_KEY,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+};
