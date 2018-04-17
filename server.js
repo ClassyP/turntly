cad3e2fe15ee17c47e0cca648f8af62b1752d47d
William Robertson <williamp.robertson@gmail.com>
2018-04-16 18:49:45 -0700

    chatbox appearing in chatcard, no funcationality

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const socket = require("socket.io");
+
 //This is our environmental variable requirement
 //const dotenv = require("dotenv");
 //const MongoClient = require("mongodb").MongoClient;
@@ -10,10 +12,10 @@ const f = require("util").format;
 //Assert is used to write unit tests
 //const assert = require("assert");
 const PORT = process.env.PORT || 3001;
-const keys = require('./config/keys.js');
+const keys = require("./config/keys.js");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(bodyParser.json());
 app.use(
@@ -25,8 +27,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
+// setup socket.io
+io = socket(server);
+
+io.on("connection", socket => {
+  console.log(socket.id);
+
+  socket.on("SEND_MESSAGE", function(data) {
+    io.emit("RECEIVE_MESSAGE", data);
+  });
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -35,11 +47,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //This is our server listener
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
