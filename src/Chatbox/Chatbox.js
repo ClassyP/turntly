2204bf8977490fcde83f080215628333c985609f
William Robertson <williamp.robertson@gmail.com>
2018-04-16 19:12:05 -0700

    testing widget

--- src/Chatbox/Chatbox.js
+++ src/Chatbox/Chatbox.js
@@ -1,86 +1,19 @@
 import React from "react";
-import io from "socket.io-client";
+import { Widget, addResponseMessage } from "react-chat-widget";
 import "./Chatbox.css";
 
 export default class Chatbox extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: "",
-      message: "",
-      messages: []
-    };
-
-    this.socket = io("localhost:8080");
-
-    this.socket.on("RECEIVE_MESSAGE", function(data) {
-      addMessage(data);
-    });
-
-    const addMessage = data => {
-      console.log(data);
-      this.setState({ messages: [...this.state.messages, data] });
-      console.log(this.state.messages);
-    };
-
-    this.sendMessage = ev => {
-      ev.preventDefault();
-      this.socket.emit("SEND_MESSAGE", {
-        author: this.state.username,
-        message: this.state.message
-      });
-      this.setState({ message: "" });
-    };
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
   }
+  handleNewUserMessage = newMessage => {
+    console.log(`New message incomig! ${newMessage}`);
+    // Now send the message throught the backend API
+  };
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="card">
-              <div className="card-body">
-                <div className="card-title">Turntly</div>
-                <hr />
-                <div className="messages">
-                  {this.state.messages.map(message => {
-                    return (
-                      <div>
-                        {message.author}: {message.message}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="footer">
-                  <input
-                    type="text"
-                    placeholder="Username"
-                    value={this.state.username}
-                    onChange={ev =>
-                      this.setState({ username: ev.target.value })
-                    }
-                    className="form-control"
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="Message"
-                    className="form-control"
-                    value={this.state.message}
-                    onChange={ev => this.setState({ message: ev.target.value })}
-                  />
-                  <br />
-                  <button
-                    onClick={this.sendMessage}
-                    className="btn btn-primary form-control"
-                  >
-                    Send
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="Chatbox">
+        <Widget handleNewUserMessage={this.handleNewUserMessage} />
       </div>
     );
   }
