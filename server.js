d5f5a28837ae2b1cb0b018317ef207ce8704fd44
Devin Collier <devinlcollier@gmail.com>
2018-04-17 16:49:02 -0700

    added mongo chat code, set .vscode to be removed in .gitignore, added mongo chat to server.js, addeed socket.io package

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const keys = require('./config/keys.js');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const chat = require("./chat/chat.js")(app);
 
 dotenv.load();
 
@@ -35,10 +36,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`Server now on port ${PORT}!`);
 });
